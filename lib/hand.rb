32902d8fa5f8a8b9324b5c2848623f1bf06ceb1a
App Academy Student <student@appacademy.io>
2014-06-13 18:08:57 -0700

    Refactoring

--- lib/hand.rb
+++ lib/hand.rb
@@ -42,61 +42,39 @@ class Hand
   private
   
   def straight
-    cards = @cards.map do |card|
-      card.number
-    end
-    cards.sort.each_with_index do |card, idx|
+    cards_numbers = @cards.map { |card| card.number }
+    cards_numbers.sort.each_with_index do |card, idx|
       next if idx == 0
-      return false if card - cards[idx-1] != 1
+      return false unless card - cards_numbers[idx-1] == 1
     end
     [4, high_card]
   end
   
   def flush
-    card_suits = @cards.map do |card| 
-      card.suit
-    end
+    card_suits = @cards.map { |card| card.suit }
     return [5, high_card] if card_suits.uniq.count == 1
     false
   end
     
   def multiples
-    card_count = @cards.inject(Hash.new{|h,k| h[k] = []}) do |card_count, card|
-      card_count[card.number] << card
-      card_count
-    end
-
-    
     # card_count is an array from numbers to arrays of cards
+    card_count = Hash.new { |h, k| h[k] = [] }
+    @cards.each { |card| card_count[card.number] << card }
 
     multiples = []
-    card_count.values.each do |value| 
-      if value.count > 1
-        multiples << value 
-      end
-    end
+    card_count.values.each { |value|  multiples << value if value.count > 1 }
 
-    fours = multiples.find {|card_set| card_set.count == 4}
-    if !fours.nil?
-      return [7,fours.max] # four of a kind
-    end
-    threes = multiples.find {|card_set| card_set.count == 3}
+    fours = multiples.find { |card_set| card_set.count == 4 }
+    return [7,fours.max] if !fours.nil?                    # four of a kind
 
-    twos = multiples.select {|card_set| card_set.count == 2}.flatten
-    # p twos
-    if !threes.nil? && !twos.empty?
-      return [6, threes.max] # full house
-    end
-    if !threes.nil?
-      return [3, threes.max] # three of a kind
-    end
-    if twos.count == 4
-      return [2, twos.max] # two pair
-    end
-    if !twos.empty?
-      return [1, twos.max] # pair
-    end
-    [0,high_card] # high card
+    threes = multiples.find { |card_set| card_set.count == 3 }
+    twos = multiples.select { |card_set| card_set.count == 2 }.flatten
+    return [6, threes.max] if !threes.nil? && !twos.empty? # full house
+    return [3, threes.max] if !threes.nil?                 # three of a kind
+    return [2, twos.max] if twos.count == 4                # two pair
+    return [1, twos.max] if !twos.empty?                   # pair
+
+    [0,high_card]                                          # high card
   end
   
   def high_card
